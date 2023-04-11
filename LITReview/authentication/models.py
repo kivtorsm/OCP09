cac448af6c236983e36f273819c1a438208ef2b0
kivtor <112388316+kivtorsm@users.noreply.github.com>
2023-04-11 14:36:15 +0200

    initial commit

---
+++ LITReview/authentication/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = None
+
+    USERNAME_FIELD = 'email'
+
