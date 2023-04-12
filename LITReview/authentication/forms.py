70463a2ff9293e0f8faf2144a22c39a034cec9d9
kivtor <112388316+kivtorsm@users.noreply.github.com>
2023-04-12 15:46:39 +0200

    login

--- LITReview/authentication/forms.py
+++ LITReview/authentication/forms.py
@@ -2,6 +2,11 @@ from django import forms
 
 
 class LoginForm(forms.Form):
+
+    USER = 'USER'
+
+    ROLE_CHOICES = (
+        (USER, 'Utilisateur'),
+    )
     username = forms.CharField(max_length=63, label='Nom d’utilisateur')
-    email = forms.EmailField(max_length=200, label=' E-mail')
     password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='Mot de passe')
--- LITReview/authentication/models.py
+++ LITReview/authentication/models.py
@@ -4,4 +4,7 @@ from django.db import models
 
 class User(AbstractUser):
     username = models.CharField(max_length=50, unique=True)
-    email = models.EmailField(unique=True)
+
+
+    # def __str__(self):
+    #     return f"{self.username} - {self.password}"
