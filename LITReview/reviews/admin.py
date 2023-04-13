51067b9804988da3e051c1068a3512d379091c9a
kivtor <112388316+kivtorsm@users.noreply.github.com>
2023-04-13 15:02:38 +0200

    correction: import syntax

--- LITReview/reviews/admin.py
+++ LITReview/reviews/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 
-from authentication.models import User
 from reviews.models import Ticket, Review, UserFollows
 
 
@@ -12,15 +11,10 @@ class ReviewAdmin(admin.ModelAdmin):
     list_display = ('ticket', 'headline', 'rating', 'user', 'time_created')
 
 
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email')
-
-
 class UserFollowsAdmin(admin.ModelAdmin):
     list_display = ('user', 'followed_user')
 
 
-admin.site.register(User, UserAdmin)
 admin.site.register(Ticket, TicketAdmin)
 admin.site.register(Review, ReviewAdmin)
 admin.site.register(UserFollows, UserFollowsAdmin)
