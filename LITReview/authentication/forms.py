405260658dcea48cd13eb5edda300a49a5e3864b
kivtor <112388316+kivtorsm@users.noreply.github.com>
2023-04-13 14:24:11 +0200

    cleaned loginform

--- LITReview/authentication/forms.py
+++ LITReview/authentication/forms.py
@@ -4,11 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class LoginForm(forms.Form):
-    USER = 'USER'
 
-    ROLE_CHOICES = (
-        (USER, 'Utilisateur'),
-    )
     username = forms.CharField(max_length=63, label='Nom d’utilisateur')
     password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='Mot de passe')
 
