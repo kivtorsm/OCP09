b2318961c1ee306cffeeb76d67a31386e5ac454b
kivtor <112388316+kivtorsm@users.noreply.github.com>
2023-04-13 15:04:23 +0200

    new: show ticket in feed

--- LITReview/reviews/views.py
+++ LITReview/reviews/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from . import models
+
 
 @login_required
 def home(request):
-    return render(request, 'reviews/home.html')
+    tickets = models.Ticket.objects.all()
+    context = {'tickets': tickets}
+    return render(request, 'reviews/home.html', context)
 
