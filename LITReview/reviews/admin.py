b47c9bd108534cdad9fe590a164032e7d1ec2e28
kivtor <112388316+kivtorsm@users.noreply.github.com>
2023-04-13 14:26:50 +0200

    new: administration of objects

--- LITReview/reviews/admin.py
+++ LITReview/reviews/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from authentication.models import User
+from reviews.models import Ticket, Review, UserFollows
+
+
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'time_created')
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('ticket', 'headline', 'rating', 'user', 'time_created')
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email')
+
+
+class UserFollowsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'followed_user')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Ticket, TicketAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(UserFollows, UserFollowsAdmin)
+
