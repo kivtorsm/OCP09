34b599fdaefc27a8f0f0555094ee0b89badf2e28
kivtor <112388316+kivtorsm@users.noreply.github.com>
2023-04-14 14:35:21 +0200

    change: show tickets and reviews with filters

--- LITReview/reviews/views.py
+++ LITReview/reviews/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 from . import models
 
 
 @login_required
 def home(request):
-    tickets = models.Ticket.objects.all()
-    reviews = models.Review.objects.all()
-    context = {'tickets': tickets, 'reviews': reviews}
+    # get list of followed users to be filtered
+    followed_users = models.UserFollows.objects.filter(user=request.user).values('followed_user_id')
+
+    reviews = models.Review.objects.filter(
+        Q(user__in=followed_users) | Q(user=request.user) | Q(ticket__user=request.user)
+    )
+
+    tickets = models.Ticket.objects.filter(
+        Q(user__in=followed_users) | Q(user=request.user)
+    ).exclude(review__in=reviews)
+
+    context = {
+        'tickets': tickets,
+        'reviews': reviews
+    }
     return render(request, 'reviews/home.html', context)
 
